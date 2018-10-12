ce6e4109148bd0920fef294133cc11b955cbffc9
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-12 06:39:16 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
+  return !(word.match(/\b[aeiouAEIOU]/) == nil)
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  return text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  return !(text.match(/\A[A-Z][\w\W]*\W\z/) == nil)
 end
 
 def valid_phone_number?(phone)
-
+  return !(phone.match(/\A\D*[0-9]\D*[0-9]\D*[0-9]\D*[0-9]\D*[0-9]\D*[0-9]\D*[0-9]\D*[0-9]\D*[0-9]\D*[0-9]\D*\z/) == nil)
 end
